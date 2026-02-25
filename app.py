2cae32a37d18306902ef530c9c0ffe601984d8c6
sami <saminsx@gail.com>
2026-02-25 12:08:32 +0200

    added GET /<room>

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app=Flask(__name__)
 def home():
     return send_file("index.html")
 
+@app.route('/<room>')
+def start_chat(room):
+    room_number = request.args.get('room')
+    return send_file('resources/html/index.html')
+
 if __name__=="__main__":
     app.run()
