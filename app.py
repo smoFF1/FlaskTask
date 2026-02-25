f28f19d3695e82426e22c31e09d4921696e96413
sami <saminsx@gail.com>
2026-02-25 12:58:40 +0200

    added post funcionality, and bad route handling

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask,redirect,url_for,render_template,send_file,request
+from flask import Flask,redirect,url_for,send_file,request
 import os
 app=Flask(__name__)
 history_dir='history'
@@ -8,6 +8,7 @@ history_dir='history'
 def home():
     return send_file("index.html")
 
+#Sami
 @app.route('/<room>')
 def start_chat(room):
     return send_file('index.html')
@@ -24,20 +25,32 @@ def api_chat_get(room):
                 chat_history+=line
     return chat_history
 
+#Sami
+#saving incoming messages inside history/<room>.txt
+#if history dir does not exist, create it
 @app.route('/api/chat/<room>', methods=['POST'])
 def api_chat_post(room):
-
     os.makedirs(history_dir, exist_ok=True)
     chat_file=f"{history_dir}/{room}.txt"
     
     with open(chat_file, 'a') as file:
+
+        #create formatted date as specified
         now = datetime.now()
         formatted = now.strftime("[%Y-%m-%d %H:%M:%S]")
+
+        #extract post request body fields (username, and msg), use them to create history entry
         line=f"{formatted} {request.form.get('username')}: {request.form.get('msg')}\n"
+
+        #write the entry to the the file
+        file.write(line)
     return "Message received", 200
 
+#Sami
+#reroute unhandeled routes to home (\)
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect(url_for("home"))
+
 if __name__=="__main__":
-    app.run()
+    app.run(host="0.0.0.0",port="5000")
